6c162ce85cd6c0461d3cb5780164c7207dedb27d
ElenaCherpakova <elena.cherpakova@gmail.com>
2021-12-01 19:08:16 +0000

    client

---
+++ client.js
@@ -0,0 +1,34 @@
+const net = require("net");
+
+
+// // establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: '165.227.47.243',
+    port: 50541
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on("data", (data) => {
+    console.log('Server says: ', data);
+  });
+
+  conn.on("connect", function(){
+    console.log("Successfully connected to game server");
+  });
+
+  conn.on('connect', () => {
+    conn.write("Name: Ele");
+  });
+  
+  return conn;
+};
+
+
+
+console.log("Connecting ...");
+connect();
+
+module.exports = { connect };
