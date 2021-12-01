4a8498e6496578679c10bfca2a08f5d75be6ea97
ElenaCherpakova <elena.cherpakova@gmail.com>
2021-12-01 19:15:34 +0000

    play

--- play.js
+++ play.js
@@ -1,24 +1,2 @@
-const net = require("net");
+const  connect = require("./client")
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: 'localhost',
-    port: 50541
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-
-  conn.on("data", (data) => {
-    console.log('Server says: ', data);
-  });
-
-  return conn;
-};
-
-console.log("Connecting ...");
-connect();
-
-module.exports = connect;
