892a273cab70b119aa9a04b4c36f45e2f56cac1c
ElenaCherpakova <elena.cherpakova@gmail.com>
2021-12-01 16:33:30 +0000

    client_setup

--- play.js
+++ play.js
@@ -3,7 +3,7 @@ const net = require("net");
 // establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
-    host: '10.0.2.15',
+    host: 'localhost',
     port: 50541
   });
 
@@ -20,3 +20,5 @@ const connect = function () {
 
 console.log("Connecting ...");
 connect();
+
+module.exports = connect;
