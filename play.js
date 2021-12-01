c2882aada5c6d3b6ecc8d758c1ea1f0235fab1c3
ElenaCherpakova <elena.cherpakova@gmail.com>
2021-12-01 20:41:57 +0000

    play.js

--- play.js
+++ play.js
@@ -1,2 +1,8 @@
-const  { connect } = require("./client")
+const { connect } = require("./client");
+const { setupInput } = require("./input");
+
+console.log("Connecting ...");
+connect();
+
+setupInput();
 
