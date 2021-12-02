b28dcc17aa985ad5bd47ab5770c163eaba371e44
ElenaCherpakova <elena.cherpakova@gmail.com>
2021-12-02 01:35:41 +0000

    refactoring

--- constants.js
+++ constants.js
@@ -1,10 +1,22 @@
-const IP = '165.227.47.243'
-const PORT = 50541
+const IP = "165.227.47.243";
+const PORT = 50541;
+let moveObj = {
+  w: "up",
+  d: "right",
+  s: "down",
+  a: "left"
+};
 
-
-
+let sayingObj = {
+  r: 'catch me',
+  t: 'too slow',
+  y: 'snakes?',
+  u: 'Snakes where?'
+};
 
 module.exports = {
   IP,
-  PORT
+  PORT,
+  sayingObj,
+  moveObj
 };
